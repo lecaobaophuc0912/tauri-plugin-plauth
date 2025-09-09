86ef1891f8ce175fcb407f32191add53ff476a52
Phuc Le <lecaobaophuc@gmail.com>
2025-09-09 15:12:38 +0700

    Fix issue crash app Android sample

--- src/mobile.rs
+++ src/mobile.rs
@@ -15,7 +15,7 @@ pub fn init<R: Runtime, C: DeserializeOwned>(
     api: PluginApi<R, C>,
 ) -> crate::Result<Plauth<R>> {
     #[cfg(target_os = "android")]
-    let handle = api.register_android_plugin("", "ExamplePlugin")?;
+    let handle = api.register_android_plugin("com.plugin.plauth", "ExamplePlugin")?;
     #[cfg(target_os = "ios")]
     let handle = api.register_ios_plugin(init_plugin_plauth)?;
     Ok(Plauth(handle))
